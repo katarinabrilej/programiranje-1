ae27920b2a6db38caf739d77dff5389097112569
Katarina Brilej <katarina.brilej@student.fmf.uni-lj.si>
2018-11-19 11:52:13 +0100

    vaje 6

--- 6-definicije-tipov/vaje/podatkovni_tipi.ml
+++ 6-definicije-tipov/vaje/podatkovni_tipi.ml
@@ -21,7 +21,24 @@
  - : euro = Euro 0.4305
 [*----------------------------------------------------------------------------*)
 
+type euro = Euro of float
+type dollar = Dollar of float
 
+let euro_to_dollar(Euro e) = Dollar(e /. 0.88)
+let dollar_to_euro(Dollar d) = Euro(0.88 *. d)
+
+(*slaba verzija*)
+let dollar_to_euro_bad dollar = 0.2 *. dollar
+
+let dollar_to_euro_good dollar = 
+  match dollar with
+  | Dollar  v -> Euro (0.2 *. v)
+
+  (*
+    let Dollar v = dollar in ....
+
+    let d_to_e (Dollar v) = ....
+  *)
 
 (*----------------------------------------------------------------------------*]
  Definirajte tip [currency] kot en vsotni tip z konstruktorji za jen, funt
@@ -35,8 +52,17 @@
  - : currency = Pound 0.007
 [*----------------------------------------------------------------------------*)
 
+type currency = 
+  | Yen of float
+  | Pound of float
+  | Krona of float
 
-
+  (*zdaj imamo tri konstruktorje zato ne moremo vec razstaviti z let, moramo z match*)
+let to_pound c = 
+  match c with
+  | Yen v -> Pound (1. *. v)
+  | Pound v -> Pound v (*se ne spremeni*)
+  | Krona v -> Pound 0.
 (*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
  Želimo uporabljati sezname, ki hranijo tako cela števila kot tudi logične
  vrednosti. To bi lahko rešili tako da uvedemo nov tip, ki predstavlja celo
@@ -47,7 +73,9 @@
  [Nil] (oz. [] v Ocamlu) in pa konstruktorjem za člen [Cons(x, xs)] (oz.
  x :: xs v Ocamlu).
 [*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*)
-
+type 'a list =
+  | Empty
+  | Cons of 'a * 'a list
 (*----------------------------------------------------------------------------*]
  Definirajte tip [intbool_list] z konstruktorji za:
   1.) prazen seznam,
@@ -57,22 +85,64 @@
  Nato napišite testni primer, ki bi predstavljal "[5; true; false; 7]".
 [*----------------------------------------------------------------------------*)
 
+type intbool_list = 
+  | Empty
+  | Integer of int * intbool_list
+  | Bool of bool * intbool_list
+  (*Int_val, Bool_val*)
+  (*vsi ti konstruktorji sprejmejo natanko en argument*)
 
+  let primer = Integer(5, Bool(true, Bool(false, Integer(7,Empty)))) 
 
 (*----------------------------------------------------------------------------*]
  Funkcija [intbool_map f_int f_bool ib_list] preslika vrednosti [ib_list] v nov
  [intbool_list] seznam, kjer na elementih uporabi primerno od funkcij [f_int]
  oz. [f_bool].
 [*----------------------------------------------------------------------------*)
+(*kako bi napisali navaden map*)
+let rec map f = function
+  | [] -> []
+  | x :: xs -> (f x) :: map f xs
+(*
+let rec map ugly f = function
+  | Empty -> Empty
+  | Cons(x, xs) -> Cons( f x, map_ugly f xs)
+*)
 
-let rec intbool_map = ()
+
+let rec intbool_map (f_int : int -> int) (f_bool: bool -> bool) = function
+  | Empty -> Empty
+  | Integer (i,xs) -> Integer(f_int i, intbool_map f_int f_bool xs)
+  | Bool (i,xs) -> Bool(f_bool i, intbool_map f_int f_bool xs)
+
+
+
+(*
+let rec intbool_map (f_int : int -> int) (f_bool: bool -> bool) = function
+  | Empty -> Empty
+  | Integer (i,tail) -> 
+      let new_i = f_int i in
+      let new_tail = intbool_map f_int f_bool tail in
+      Integer (new_i, new_tail)
+*)  
 
 (*----------------------------------------------------------------------------*]
  Funkcija [intbool_reverse] obrne vrstni red elementov [intbool_list] seznama.
  Funkcija je repno rekurzivna.
 [*----------------------------------------------------------------------------*)
 
-let rec intbool_reverse = ()
+let rec intbool_reverse ib_list =
+  let rec reverse' (acc: intbool_list) = function (*povemu mo da je akumulator ipa intbool_list*)
+    | Empty -> acc
+    | Integer(i, tail) -> 
+        let new_acc = Integer(i,acc) in 
+        reverse' new_acc tail
+    | Bool (b, tail) ->
+        let new_acc = Bool(b,acc) in 
+        reverse' new_acc tail
+  in
+  reverse' Empty ib_list
+
 
 (*----------------------------------------------------------------------------*]
  Funkcija [intbool_separate ib_list] loči vrednosti [ib_list] v par [list]
@@ -80,7 +150,15 @@ let rec intbool_reverse = ()
  vrednosti. Funkcija je repno rekurzivna in ohranja vrstni red elementov.
 [*----------------------------------------------------------------------------*)
 
-let rec intbool_separate = ()
+let rec intbool_separate ib_list = 
+  let rec separate' acc1 acc2  = function
+  | Empty -> (acc1, acc2)
+  | Integer(i, tail) -> separate' (acc1 @ [i]) acc2 tail
+  | Bool(b, tail) -> separate' acc1 (acc2 @ [b])  tail
+
+  in
+  separate' [] [] ib_list
+
 
 (*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
  Določeni ste bili za vzdrževalca baze podatkov za svetovno priznano čarodejsko
