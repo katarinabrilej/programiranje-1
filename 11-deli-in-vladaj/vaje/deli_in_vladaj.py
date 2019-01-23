0f69ff692dd140c1d1fd6b79c72ac1a04d3409b0
Katarina Brilej <katarina.brilej@gmail.com>
2019-01-23 12:49:56 +0100

    deli in vladaj

--- 11-deli-in-vladaj/vaje/deli_in_vladaj.py
+++ 11-deli-in-vladaj/vaje/deli_in_vladaj.py
@@ -47,6 +47,29 @@ def pivot(a,start,end):
 
 a = [10, 4, 5, 15, 11, 2, 17, 0, 18]
 
+# uradna rešitev
+def pivot(a, start, end):
+    # save pivot
+    pivot = a[start]
+    # save pointers
+    front_i = start
+    back_i = end
+    # move pointers and change elements if needed
+    while front_i != back_i:
+        if a[front_i + 1] <= pivot:
+            front_i += 1
+        elif a[back_i] > pivot:
+            back_i -= 1
+        else:
+            temp = a[front_i + 1]
+            a[front_i + 1] = a[back_i]
+            a[back_i] = temp
+    # move pivot
+    a[start] = a[front_i]
+    a[front_i] = pivot
+    # return the final index of pivot
+    return front_i
+
 
 ##############################################################################
 # Tabelo a želimo urediti z algoritmom hitrega urejanja (quicksort).
@@ -73,6 +96,20 @@ def quicksort_part(a, start, end):
 def quicksort(a):
     return quicksort_part(a, 0, len(a)-1)
 
+# uradna rešitev
+def quicksort_part(a, start, end):
+    if start >= end:
+        return
+    else:
+        pivot_i = pivot(a, start, end)
+        quicksort_part(a, start, pivot_i - 1)
+        quicksort_part(a, pivot_i + 1, end)
+        return
+
+
+def quicksort(a):
+    quicksort_part(a, 0, len(a) - 1)
+    return
 
 ##############################################################################
 # V tabeli želimo poiskati vrednost k-tega elementa po velikosti.
@@ -97,3 +134,23 @@ def kth_element(a,k):
         return kth_element(a[p+1:],k-p-1)
     else:
         return kth_element(a[:p],k)
+
+# uradna rešitev
+def kth_el_part(a, k, start, end):
+    if start > end:
+        return None
+    else:
+        pivot_i = pivot(a, start, end)
+        if pivot_i == k:
+            return a[pivot_i]
+        elif pivot_i > k:
+            return kth_el_part(a, k, start, pivot_i - 1)
+        else:
+            return kth_el_part(a, k, pivot_i + 1, end)
+
+
+def kth_element(a, k):
+    if k > len(a):
+        return None
+    else:
+        return kth_el_part(a, k, 0, len(a)-1)
